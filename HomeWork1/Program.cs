026bcecb21b050da8535a9c7f6a3c9b070497b54
Mitarius <mitarius@gmail.com>
2021-05-16 23:35:43 +0200

    comments for HW addedhw ready

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HW3 "Картинки"
+            int rowSize = 3;
+            int imageCount = 52;
+            int fullRowCount = imageCount / rowSize;
+            int remainingImages = imageCount % rowSize;
+
+            Console.WriteLine($"Заполненных рядов: {fullRowCount}, остаток: {remainingImages}");
+            Console.ReadKey();
+        }
+    }
+}
