4ba3c593b0682d29b17f84b34468c3acaf475712
Mitarius <mitarius@gmail.com>
2021-05-16 23:35:43 +0200

    hw ready

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowSize = 3;
+            int imageCount = 52;
+            int fullRowCount = imageCount / rowSize;
+            int remainingImages = imageCount % rowSize;
+
+            Console.WriteLine($"Заполненных рядов: {fullRowCount}, остаток: {remainingImages}");
+            Console.ReadKey();
+        }
+    }
+}
