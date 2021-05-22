c1b2f8378c138ababb2d86d2a1471ad45f6f4b4e
Mitarius <mitarius@gmail.com>
2021-05-22 22:36:35 +0200

    HW 08 is done

---
+++ HomeWork08/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeWork08
+{   /// <summary>
+    /// Домашнее задание "Контроль выхода"
+    /// Написать программу, которая будет выполняться до тех пор, пока не будет введено слово exit.
+    ///Помните, в цикле должно быть условие, которое отвечает за то, когда цикл должен завершиться.
+    ///Это нужно, чтобы любой разработчик взглянув на ваш код, понял четкие границы вашего цикла.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exitCommand = "Exit";
+            string userCommand = "";
+            while (userCommand != exitCommand)
+            {
+                Console.Write($"Please, enter your command, or {exitCommand} to quit: ");
+                userCommand = Console.ReadLine();
+                Console.WriteLine($"You have entered: {userCommand}");
+            }
+            
+        }
+    }
+}
