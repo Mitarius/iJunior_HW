36cb312b228607823b067c57213578c82277021a
Mitarius <mitarius@gmail.com>
2021-05-24 22:06:16 +0200

    HomwWork12 is ready

---
+++ HomeWork12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HomeWork12
+{
+    /// <summary>
+    /// Домашняя работа "вывод имени"
+    /// Вывести имя в прямоугольник из символа, который введет сам пользователь.
+    /// Вы запрашиваете имя, после запрашиваете символ, а после отрисовываете в консоль его имя в прямоугольнике из его символов.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            string decoratorSymbol;
+
+            Console.Write("Enter your name:");
+            userName = Console.ReadLine();
+            Console.WriteLine("Please enter decorator symbol");
+            decoratorSymbol = Console.ReadLine();
+
+            for (int i = 0; i <= userName.Length + 1; i++) 
+            {
+                Console.Write(decoratorSymbol);
+            }
+            Console.WriteLine("\n"+decoratorSymbol + userName + decoratorSymbol);
+
+            for (int i = 0; i <= userName.Length + 1; i++)
+            {
+                Console.Write(decoratorSymbol);
+            }
+
+        }
+    }
+}
